c24868a074863dacd701d3124da320e0fe409500
MickyBalladelli <micky.balladelli@gmail.com>
2026-03-25 20:07:47 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const satelliteWasmStub = {
+  name: 'satellite-wasm-stub',
+  enforce: 'pre',
+  resolveId(id) {
+    if (id === '#wasm-single-thread' || id === '#wasm-multi-thread') {
+      return '\0satellite-wasm-stub'
+    }
+  },
+  load(id) {
+    if (id === '\0satellite-wasm-stub') {
+      return 'export default null'
+    }
+  },
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), satelliteWasmStub],
   optimizeDeps: {
     exclude: ['satellite.js'],
   },
